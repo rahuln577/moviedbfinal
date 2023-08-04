a685da5168d2237249a7ca546abdf11892f6cbb0
rahuln577 <rahulrahuln2001@gmail.com>
2023-08-05 02:19:46 +0530

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { SiThemoviedatabase } from 'react-icons/si';
 import {BiSearchAlt} from 'react-icons/bi'
+import {useState} from "react"
 import {Link} from "react-router-dom"
 export default function Navbar(){
 const style={
@@ -11,6 +12,10 @@ const styleser={
     fontSize:"2rem",
     color:"white",
 }
+let [show,setshow]=useState(false)
+function handle(){
+    setshow(!show)
+}
 
 return <div id="nav"><nav>
     <Link to="/" ><SiThemoviedatabase style={style}/></Link>
@@ -21,6 +26,19 @@ return <div id="nav"><nav>
     <Link className="eleme"to="/toprated" state={{url:"https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1"}}>Top Rated</Link>
     <Link className="eleme"to="/upcoming" state={{url:"https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1"}}>Upcoming</Link>
     </div>
+    <div className="tod">
+    <div id="holder" onClick={handle}>
+        <div className="ico"></div>
+        <div className="ico"></div>
+        <div className="ico"></div>
+    </div>
+    </div>
 </nav>
+<div id="ham" className={show?"show":"hide"}>
+    <Link onClick={handle} className="eleme1"to="/nowplaying" state={{url:"https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1"}}>Now Playing</Link>
+    <Link onClick={handle} className="eleme1"to="/popular" state={{url:"https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"}}>Popular</Link>
+    <Link onClick={handle} className="eleme1"to="/toprated" state={{url:"https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1"}}>Top Rated</Link>
+    <Link onClick={handle} className="eleme1"to="/upcoming" state={{url:"https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1"}}>Upcoming</Link>
+    </div>
 </div>
 }
